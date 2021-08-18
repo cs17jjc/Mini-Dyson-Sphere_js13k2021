63e3c9b8fb0dd92c77be14c453eec4e7a810fc0a
cs17jjc <quantumphysicslhc.cuth@gmail.com>
2021-08-18 03:29:10 +0100

    Refactored cloud generation, added central star

--- main.js
+++ main.js
@@ -61,6 +61,8 @@ cloud = (x, y, r) => { return defObejct("cloud", x, y, { r }); }
 
 reciver = (x, y) => { return defObejct("reciver", x, y, { r: 10 }); }
 
+star = (x, y, r) => { return defObejct("star", x, y, { r }); }
+
 //is point in rectangle
 isPinR = (obj, rect) => {
     return (obj.x >= rect.x && obj.x <= rect.x + rect.w && obj.y >= rect.y && obj.y <= rect.y + rect.h);
@@ -84,6 +86,8 @@ angleBetw = (p1, p2) => {
 mag = (p) => { return Math.sqrt(Math.pow(p.x, 2) + Math.pow(p.y, 2)); }
 
 lerp = (a, b, t) => { return a + (b - a) * t; }
+
+rnd = (a, b) => { return lerp(a, b, Math.random()); }
 
 
 //?QUADTREE
@@ -177,13 +181,16 @@ var target = { x: 0, y: 0 };
 var scale = 1;
 var wOff = { x: MAP_WIDTH / 2 - (rCanv.width * scale) / 2, y: MAP_HEIGHT / 2 - (rCanv.height * scale) / 2 };
 
-genCloudCluster = (x, y) => {
-    var r = Math.random() * 10 + 50;
+var paused = false;
+var frame = 0;
+
+genCloudCluster = (x, y, r) => {
     var cloudCluster = [];
-    var n = Math.floor(Math.random() * 3) + 5;
+    var n = Math.floor(rnd(5, 8));
     for (var i = 0; i < n; i++) {
-        var cx = Math.cos((Math.PI * 2) / n * i) * lerp(r * 0.5, r, Math.random());
-        var cy = Math.sin((Math.PI * 2) / n * i) * lerp(r * 0.5, r, Math.random());
+        var tempR = rnd(r * 0.5, r * 0.8);
+        var cx = Math.cos((Math.PI * 2) / n * i) * tempR;
+        var cy = Math.sin((Math.PI * 2) / n * i) * tempR;
         cloudCluster.push(cloud(cx + x, cy + y, mag({ x: cx, y: cy })));
     }
     return cloudCluster;
@@ -191,12 +198,35 @@ genCloudCluster = (x, y) => {
 
 //?GENERATE MAP
 
-while (clouds.length < 50) {
-    var nextP = { x: Math.random() * MAP_WIDTH, y: Math.random() * MAP_HEIGHT };
-    if (!clouds.some(c => { return dist(c, nextP) < c.data.r * 5 })) {
-        clouds = clouds.concat(genCloudCluster(nextP.x, nextP.y));
+
+stars.push(star(MAP_WIDTH / 2, MAP_HEIGHT / 2, 25));
+
+//Generate clouds
+//[radius, number, minR, maxR]
+[
+    [270, 2, 50, 50],
+    [200, 5, 20, 20],
+    [380, 2, 80, 80],
+].forEach(layout => {
+    for (var i = 0; i < layout[1]; i++) {
+        var counter = 0;
+        var hasPlace = false;
+        while (counter < 100 && !hasPlace) {
+            var ang = rnd(0, Math.PI * 2);
+            var r = layout[0];
+            var nextP = { x: MAP_WIDTH / 2 + Math.cos(ang) * r, y: MAP_HEIGHT / 2 + Math.sin(ang) * r };
+
+            var radius = rnd(layout[2], layout[3]);
+            if (!clouds.some(c => { return dist(c, nextP) < c.data.r * 2 + radius * 2; })) {
+                clouds = clouds.concat(genCloudCluster(nextP.x, nextP.y, radius));
+                hasPlace = true;
+            }
+
+            counter++;
+        }
     }
-}
+});
+
 
 //?UPDATE
 update = () => {
@@ -334,8 +364,15 @@ update = () => {
     mKeysPrev = new Map(mKeys);
     prevMPos = mPos;
     prevClick = click;
-    scale -= 0.0001;
-    wOff = { x: MAP_WIDTH / 2 - (rCanv.width * scale) - rCanv.width / 2, y: MAP_HEIGHT / 2 - (rCanv.height * scale) - rCanv.height / 2 };
+    if (!paused) {
+        frame++;
+        scale = Math.max(0.5, scale - 0.0001);
+        wOff = { x: MAP_WIDTH / 2 - (rCanv.width * scale) - rCanv.width / 2, y: MAP_HEIGHT / 2 - (rCanv.height * scale) - rCanv.height / 2 };
+        mPos = {
+            x: ((rawMPos.x - dspCanvRect.left) / (dspCanvRect.right - dspCanvRect.left) * rCanv.width / scale) - wOff.x / scale,
+            y: ((rawMPos.y - dspCanvRect.top) / (dspCanvRect.bottom - dspCanvRect.top) * rCanv.height / scale) - wOff.y / scale
+        };
+    }
 }
 
 //?RENDER
@@ -401,6 +438,14 @@ render = () => {
         ctx.fill();
     });
 
+    //Render stars
+    stars.forEach(obj => {
+        ctx.beginPath();
+        ctx.arc(obj.x, obj.y, obj.data.r, 0, 2 * Math.PI);
+        ctx.fillStyle = "#FFFF00";
+        ctx.fill();
+    });
+
     ctx.beginPath();
     ctx.arc(mPos.x, mPos.y, 3, 0, 2 * Math.PI);
     ctx.fillStyle = "#00FF00";
@@ -410,9 +455,11 @@ render = () => {
 }
 
 //?INPUT
+var rawMPos = { x: 0, y: 0 };
 var mPos = { x: 0, y: 0 };
 var prevMPos = { x: 0, y: 0 };
 window.onmousemove = (e) => {
+    rawMPos = { x: e.clientX, y: e.clientY };
     mPos = {
         x: ((e.clientX - dspCanvRect.left) / (dspCanvRect.right - dspCanvRect.left) * rCanv.width / scale) - wOff.x / scale,
         y: ((e.clientY - dspCanvRect.top) / (dspCanvRect.bottom - dspCanvRect.top) * rCanv.height / scale) - wOff.y / scale
